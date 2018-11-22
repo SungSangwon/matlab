493e712cc870cd3a3ddcee0665f70634bd224bd6
SungSangwon <44973398+SungSangwon@users.noreply.github.com>
2018-11-22 16:28:12 +0900

    Update odesimp.m

--- Function Functions/odesimp.m
+++ Function Functions/odesimp.m
@@ -1,9 +1,11 @@
-function yend = odesimp(dydt, dt, ti, tf, yi)
-t= ti; y= yi; h=dt;
+function yend = odesimp(dydt,dt,ti,tf,yi, varargin)
+t = ti; y=yi; h=dt;
 while(1)
-    if t+dt>tf, h= tf-t; end
-    y=y+dydt(t)*h;
+    if t+dt>tf, h=tf-t;
+    end
+    y=y+dydt(t,varargin{:})*h;
     t=t+h;
     if t>=tf, break, end
 end
-yend=y; 
+yend =y;
+end
